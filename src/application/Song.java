ccffce4e059f8e01b92373ff2bc77a1f23352dae
shivp124 <82459557+shivp124@users.noreply.github.com>
2023-02-09 21:31:28 -0500

    Update Song.java

--- src/application/Song.java
+++ src/application/Song.java
@@ -1,10 +1,17 @@
 package application;
 
-public class Song {
+public class Song implements Comparable<Song>{
     private String title;
     private String artist;
     private String album;
     private String year;
+
+    public Song() {
+        this.title = "";
+        this.artist = "";
+        this.album = "";
+        this.year = "";
+    }
 
     public Song(String title, String artist, String album, String year) {
         this.title = title;
@@ -48,5 +55,14 @@ public class Song {
     @Override
     public String toString() {
         return title + " by " + artist;
+    }
+
+    @Override
+    public int compareTo(Song s) {
+        if(this.title.toLowerCase().compareTo(s.title.toLowerCase()) == 0) {
+            return this.artist.toLowerCase().compareTo(s.artist.toLowerCase());
+        } else {
+            return this.title.toLowerCase().compareTo(s.title.toLowerCase());
+        }
     }
 }
